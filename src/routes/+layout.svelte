d68ae2d8f3803e30dc946562f2c813456f6440de
Francis Dominic Fajardo <fajardo@duck.com>
2026-04-15 22:22:49 +0800

    refactor: generate pageInfo from root layout.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,13 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
+	let { data, children } = $props();
+	const { pageInfo } = $derived(data);
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<title>{pageInfo?.title ? pageInfo.title + ' - HAOne' : 'HAOne'}</title>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
 {@render children()}
