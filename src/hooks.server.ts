55841a063f9fb997028681cdd34a053dc0407339
Francis Dominic Fajardo <fajardo@duck.com>
2026-04-19 02:48:07 +0800

    feat: add GA support.

---
+++ src/hooks.server.ts
@@ -0,0 +1,25 @@
+import { PUBLIC_GA_ID } from "$env/static/public";
+import type { Handle } from "@sveltejs/kit";
+
+export const handle: Handle = async ({ event, resolve }) => {
+  return await resolve(event, {
+    transformPageChunk: ({ html }) => {
+      if (PUBLIC_GA_ID) {
+        const gaScript = `
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=${PUBLIC_GA_ID}"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+
+      gtag("config", "${PUBLIC_GA_ID}");
+    </script>`;
+        return html.replace("%google_analytics%", gaScript);
+      }
+      return html.replace("%google_analytics%", "");
+    }
+  });
+};
