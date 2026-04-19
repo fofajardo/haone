bd2c455125164f5a42a7334fc6cbc9ea0fb86ae7
Francis Dominic Fajardo <fajardo@duck.com>
2026-04-20 02:09:09 +0800

    ui: add missing page info.

--- src/routes/clearance/+page.server.ts
+++ src/routes/clearance/+page.server.ts
@@ -2,5 +2,10 @@ import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ url }) => {
   const data = url.searchParams.get("data");
-  return { data };
+  return {
+    data,
+    pageInfo: {
+      title: "Residence Hall Association Clearance"
+    }
+  };
 };
