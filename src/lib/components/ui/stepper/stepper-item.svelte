63b5dfaa3654fa3bb76bba7875ea30a5c857eaaa
Francis Dominic Fajardo <fajardo@duck.com>
2026-07-22 23:44:08 +0800

    fix: stepper tracking.

--- src/lib/components/ui/stepper/stepper-item.svelte
+++ src/lib/components/ui/stepper/stepper-item.svelte
@@ -7,7 +7,11 @@
 
   let { id = uid, class: className, children, ...rest }: StepperItemProps = $props();
 
-  const stepperItemState = useStepperItem({ id });
+  const stepperItemState = useStepperItem({
+    get id() {
+      return id;
+    }
+  });
 </script>
 
 <div
