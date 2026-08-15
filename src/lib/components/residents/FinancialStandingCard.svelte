76b8611c1b7cf66c4fab110a745a94f02208ac7f
Francis Dominic Fajardo <fajardo@duck.com>
2026-08-15 21:04:17 +0800

    fix: remove duplicate class.

--- src/lib/components/residents/FinancialStandingCard.svelte
+++ src/lib/components/residents/FinancialStandingCard.svelte
@@ -67,7 +67,7 @@
         </div>
         <div class="flex flex-col gap-0.5">
           <span>Paid</span>
-          <span class="text-foreground text-primary">{formatAmount(account.assocPaid)}</span>
+          <span class="text-foreground">{formatAmount(account.assocPaid)}</span>
         </div>
         <div class="flex flex-col gap-0.5">
           <span>Waived</span>
