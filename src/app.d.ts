1d2b90e19a30ccdf0519f62e0729b82d3caca990
Francis Dominic Fajardo <fajardo@duck.com>
2026-09-09 19:09:49 +0800

    fix: declare types for @compwright/namecase.

--- src/app.d.ts
+++ src/app.d.ts
@@ -20,4 +20,18 @@ declare global {
   }
 }
 
+// XXX: Remove these once the namecase package is updated to include its own types.
+declare module "@compwright/namecase" {
+  export interface NamecaseOptions {
+    individualFields?: boolean;
+  }
+
+  export function checkName(name: string): boolean;
+  export function normalize(name: string): string;
+
+  export function namecase(input: string, opt?: NamecaseOptions): string;
+  export function namecase(input: string[], opt?: NamecaseOptions): string[];
+  export function namecase<T>(input: T, opt?: NamecaseOptions): T;
+}
+
 export {};
