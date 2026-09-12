5e75c7354b232acb11568268bf27d726cd710c71
brickwall2900 <brickwall2900@gmail.com>
2026-09-13 07:36:34 +0800

    docs: add documentation for isFeatureFlagEnabled

--- src/lib/api/utils/feature-flags.ts
+++ src/lib/api/utils/feature-flags.ts
@@ -1,5 +1,11 @@
 import { CONSTANT_COL } from "$lib/types";
 
+/**
+ * checks if the feature flag is enabled by searching the given constant rows.
+ * @param constantRows constant rows that was retrieved from the constants table (spreadsheet) (database or whatever)
+ * @param theFeatureFlag the feature flag to look for
+ * @returns `true` if the feature flag is set to enabled, `false` otherwise. it returns `true` if not set in the constants table.
+ */
 export function isFeatureFlagEnabled(constantRows: any[][], theFeatureFlag: string): boolean {
   const featureFlag = constantRows.find((r: any) => {
     return (
